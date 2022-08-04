71bda19ae6390cba819cd8b387b8ab07bf215514
rafael <rafael.conte@webjump.com.br>
2022-08-04 09:13:28 -0300

    Subindo o projeto para o git

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,18 @@
+describe('cadastro de usuario', ()=>{
+    it('cadastro de usuario com sucesso', ()=>{
+    
+        cy.visit('https://magento.nublue.co.uk/')
+        cy.get('div[class="panel header"] a[href="https://magento.nublue.co.uk/customer/account/create/"]').click()
+        cy.get('#firstname').type('Nilay')
+        cy.get('#lastname').type('Conte')
+        cy.get('input[id="is_subscribed"]').click()
+        cy.get('#assistance_allowed_checkbox').click()
+        cy.get('div[class="field required"] input[type="email"]').type('nilay_conte23@wj.com')
+        cy.get('input[id="password"]').type('134679rA')
+        cy.get('input[id="password-confirmation"]').type('134679rA')
+        cy.get('button[class="action submit primary"]').click()
+        var mensagem = 'Thank you for registering with Main Website Store.'
+        cy.contains('div[role="alert"]', mensagem).should('be.visible')
+    
+    })
+})
