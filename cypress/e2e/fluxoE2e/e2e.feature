344174a31b46748bb9cfded9fd7a1a8e9bf8ed79
rafael conte <rafael.conte@webjump.com.br>
2022-09-20 16:40:52 -0300

    criar pastas
    Criação das pasta, elements, page objects, steps. implementação do arquivo e2e. feature

---
+++ cypress/e2e/fluxoE2e/e2e.feature
@@ -0,0 +1,21 @@
+Feature: Fluxo E2e
+  sendo um usuário cadastro no sistema
+  Posso efetuar o login no sistema
+  Para que possa realiza compras no site
+
+
+  Scenario: Realizar login e finalizar compra com fluxo completo
+    Given que o usuário acessa o site luma
+    When o usuário clica no link Sign In no cabeçalho
+    And o usuário realiza login com sucesso
+    And o usuário pesquisa por produto na barra de pesquisa
+    And o usuário escolhe produto pesquisado
+    And o usuário escolhe tamanho e cor
+    And o usuário adiciona produto no carrinho
+    And o usuário preenche os campo da página shipping and address
+    And o usuário seleciona o metodo de envio
+    And o usário seleciona o metodo de pagamento
+    And o usuário clica no botão Place order
+    Then o usuário deverá ver a mensagem "Thank you for your purchase!"
+
+
---
+++ cypress/support/steps/e2eStep.js
@@ -0,0 +1,49 @@
+
+Given(/^que o usuário acessa o site luma$/, () => {
+	return true;
+});
+
+When(/^o usuário clica no link Sign In no cabeçalho$/, () => {
+	return true;
+});
+
+When(/^o usuário realiza login com sucesso$/, () => {
+	return true;
+});
+
+When(/^o usuário pesquisa por produto na barra de pesquisa$/, () => {
+	return true;
+});
+
+When(/^o usuário escolhe produto pesquisado$/, () => {
+	return true;
+});
+
+When(/^o usuário escolhe tamanho e cor$/, () => {
+	return true;
+});
+
+When(/^o usuário adiciona produto no carrinho$/, () => {
+	return true;
+});
+
+When(/^o usuário preenche os campo da página shipping and address$/, () => {
+	return true;
+});
+
+When(/^o usuário seleciona o metodo de envio$/, () => {
+	return true;
+});
+
+When(/^o usário seleciona o metodo de pagamento$/, () => {
+	return true;
+});
+
+When(/^o usuário clica no botão Place order$/, () => {
+	return true;
+});
+
+Then(/^o usuário deverá ver a mensagem "([^"]*)"$/, (args1) => {
+	console.log(args1);
+	return true;
+});
